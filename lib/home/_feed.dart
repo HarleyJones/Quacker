fcc3c9994d12b70c67bfe17620ae5b81d05fc393
mrhcjones <150591975+mrhcjones@users.noreply.github.com>
2024-03-14 14:54:15 +0000

    Added actions to AppBar for Following tab

--- lib/home/_feed.dart
+++ lib/home/_feed.dart
@@ -3,6 +3,7 @@ import 'package:pref/pref.dart';
 import 'package:provider/provider.dart';
 import 'package:quacker/forYou/_tweets.dart';
 import 'package:quacker/generated/l10n.dart';
+import 'package:quacker/group/_settings.dart';
 import 'package:quacker/group/group_model.dart';
 import 'package:quacker/group/group_screen.dart';
 import 'package:quacker/home/home_screen.dart';
@@ -26,6 +27,25 @@ class _FeedScreenState extends State<FeedScreen>
 
   UserWithExtra user = UserWithExtra();
 
+  late TabController _tabController;
+  int _tab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.animation!.addListener(_tabListener);
+    WidgetsBinding.instance.addPostFrameCallback((_) => print("hi"));
+  }
+
+  void _tabListener() {
+    if (_tab != _tabController.animation!.value.round()) {
+      setState(() {
+        _tab = _tabController.animation!.value.round();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -34,33 +54,42 @@ class _FeedScreenState extends State<FeedScreen>
     user.possiblySensitive = false;
     user.screenName = "ForYou";
 
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-            appBar: AppBar(
-                title: Text(L10n.current.feed),
-                bottom: TabBar(
-                  tabs: [Tab(child: Text(L10n.current.foryou)), Tab(child: Text(L10n.current.following))],
-                )),
-            body: TabBarView(
-              children: [
-                ForYouTweets(
-                    user: user,
-                    type: 'profile',
-                    includeReplies: false,
-                    pinnedTweets: [],
-                    pref: PrefService.of(context)),
-                Provider<GroupModel>(create: (context) {
-                  var model = GroupModel(widget.id);
-                  model.loadGroup();
+    dynamic forYouTweets = ForYouTweets(
+        user: user, type: 'profile', includeReplies: false, pinnedTweets: [], pref: PrefService.of(context));
 
-                  return model;
-                }, builder: (context, child) {
-                  return SubscriptionGroupScreenContent(
-                    id: widget.id,
-                  );
-                })
-              ],
-            )));
+    return Scaffold(
+        body: Provider<GroupModel>(create: (context) {
+      var model = GroupModel(widget.id);
+      model.loadGroup();
+
+      return model;
+    }, builder: (context, child) {
+      var model = context.read<GroupModel>();
+
+      return Scaffold(
+          appBar: AppBar(
+              title: Text(L10n.current.feed),
+              actions: _tab == 0
+                  ? [...createCommonAppBarActions(context)]
+                  : [
+                      IconButton(icon: const Icon(Icons.more_vert), onPressed: () => showFeedSettings(context, model)),
+                      IconButton(
+                          icon: const Icon(Icons.refresh_rounded),
+                          onPressed: () async {
+                            await model.loadGroup();
+                          }),
+                      ...createCommonAppBarActions(context)
+                    ],
+              bottom: TabBar(
+                controller: _tabController,
+                tabs: [Tab(child: Text(L10n.current.foryou)), Tab(child: Text(L10n.current.following))],
+              )),
+          body: TabBarView(controller: _tabController, children: [
+            forYouTweets,
+            SubscriptionGroupScreenContent(
+              id: widget.id,
+            ),
+          ]));
+    }));
   }
 }
