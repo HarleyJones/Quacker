9eb7aa45ac12f71c8aa75e78138fa16d42138c30
mrhcjones <150591975+mrhcjones@users.noreply.github.com>
2024-03-14 15:53:09 +0000

    hide appbar on feed scroll

--- lib/home/_feed.dart
+++ lib/home/_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pref/pref.dart';
 import 'package:provider/provider.dart';
 import 'package:quacker/forYou/_tweets.dart';
@@ -57,8 +58,7 @@ class _FeedScreenState extends State<FeedScreen>
     dynamic forYouTweets = ForYouTweets(
         user: user, type: 'profile', includeReplies: false, pinnedTweets: [], pref: PrefService.of(context));
 
-    return Scaffold(
-        body: Provider<GroupModel>(create: (context) {
+    return Provider<GroupModel>(create: (context) {
       var model = GroupModel(widget.id);
       model.loadGroup();
 
@@ -66,30 +66,60 @@ class _FeedScreenState extends State<FeedScreen>
     }, builder: (context, child) {
       var model = context.read<GroupModel>();
 
-      return Scaffold(
-          appBar: AppBar(
-              title: Text(L10n.current.feed),
-              actions: _tab == 0
-                  ? [...createCommonAppBarActions(context)]
-                  : [
-                      IconButton(icon: const Icon(Icons.more_vert), onPressed: () => showFeedSettings(context, model)),
-                      IconButton(
-                          icon: const Icon(Icons.refresh_rounded),
-                          onPressed: () async {
-                            await model.loadGroup();
-                          }),
-                      ...createCommonAppBarActions(context)
-                    ],
-              bottom: TabBar(
-                controller: _tabController,
-                tabs: [Tab(child: Text(L10n.current.foryou)), Tab(child: Text(L10n.current.following))],
-              )),
+      return NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                pinned: false,
+                snap: true,
+                floating: true,
+                title: Text(L10n.current.feed),
+                actions: _tab == 0
+                    ? [...createCommonAppBarActions(context)]
+                    : [
+                        IconButton(
+                            icon: const Icon(Icons.more_vert), onPressed: () => showFeedSettings(context, model)),
+                        IconButton(
+                            icon: const Icon(Icons.refresh_rounded),
+                            onPressed: () async {
+                              await model.loadGroup();
+                            }),
+                        ...createCommonAppBarActions(context)
+                      ],
+                bottom: TabBar(
+                  controller: _tabController,
+                  tabs: [Tab(child: Text(L10n.current.foryou)), Tab(child: Text(L10n.current.following))],
+                ),
+              ),
+            ];
+          },
           body: TabBarView(controller: _tabController, children: [
             forYouTweets,
             SubscriptionGroupScreenContent(
               id: widget.id,
             ),
           ]));
-    }));
+    });
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return _tabBar;
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
