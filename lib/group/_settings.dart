8c46efd66a245005de7f0f3160a139278bd98ebb
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-04-07 12:05:46 +0100

    add: improved bottomModal for feed settings

--- lib/group/_settings.dart
+++ lib/group/_settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_triple/flutter_triple.dart';
+import 'package:pref/pref.dart';
 import 'package:quacker/database/entities.dart';
 import 'package:quacker/generated/l10n.dart';
 import 'package:quacker/group/group_model.dart';
@@ -10,63 +11,55 @@ void showFeedSettings(BuildContext context, GroupModel model) {
       builder: (context) {
         var theme = Theme.of(context);
 
-        return SizedBox(
-          height: 250,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+              title: Text(
+                L10n.of(context).filters,
+                style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      }),
-                  title: Text(L10n.of(context).filters),
-                  tileColor: theme.colorScheme.primary,
-                ),
-                Container(
-                    alignment: Alignment.centerLeft,
-                    margin: const EdgeInsets.only(bottom: 8, top: 16, left: 16, right: 16),
-                    child: Text(
-                      L10n.of(context).note_due_to_a_twitter_limitation_not_all_tweets_may_be_included,
-                      style: TextStyle(
-                        color: Theme.of(context).disabledColor,
-                      ),
-                    )),
-                ScopedBuilder<GroupModel, SubscriptionGroupGet>(
-                  store: model,
-                  onState: (_, state) {
-                    return Column(
-                      children: [
-                        CheckboxListTile(
-                            title: Text(
-                              L10n.of(context).include_replies,
-                            ),
-                            value: model.state.includeReplies,
-                            onChanged: (value) async {
-                              await model.toggleSubscriptionGroupIncludeReplies(value ?? false);
-                            }),
-                        CheckboxListTile(
-                            title: Text(
-                              L10n.of(context).include_retweets,
-                            ),
-                            value: model.state.includeRetweets,
-                            onChanged: (value) async {
-                              await model.toggleSubscriptionGroupIncludeRetweets(value ?? false);
-                            }),
-                      ],
-                    );
-                  },
-                ),
-              ],
+            Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(bottom: 8, top: 16, left: 16, right: 16),
+                child: Text(
+                  L10n.of(context).note_due_to_a_twitter_limitation_not_all_tweets_may_be_included,
+                  style: TextStyle(
+                    color: Theme.of(context).disabledColor,
+                  ),
+                )),
+            ScopedBuilder<GroupModel, SubscriptionGroupGet>(
+              store: model,
+              onState: (_, state) {
+                return Column(
+                  children: [
+                    SwitchListTile(
+                        title: Text(
+                          L10n.of(context).include_replies,
+                        ),
+                        value: model.state.includeReplies,
+                        onChanged: (value) async {
+                          await model.toggleSubscriptionGroupIncludeReplies(value ?? false);
+                        }),
+                    SwitchListTile(
+                        title: Text(
+                          L10n.of(context).include_retweets,
+                        ),
+                        value: model.state.includeRetweets,
+                        onChanged: (value) async {
+                          await model.toggleSubscriptionGroupIncludeRetweets(value ?? false);
+                        }),
+                  ],
+                );
+              },
             ),
-          ),
+          ],
         );
       });
 }
