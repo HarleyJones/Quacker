4b8b67204eb3663e495696f572760891c5df289c
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-04-11 16:43:08 +0100

    workaround: get auth header every time to avoid loading from database

--- lib/client/NEWclient_regular_account.dart
+++ lib/client/NEWclient_regular_account.dart
@@ -15,8 +15,8 @@ Future<String> addAccount(BasePrefService prefs, String username, String passwor
     final authHeader = await model.GetAuthHeader(username: username, password: password, email: email);
 
     if (authHeader != null) {
-      database.insert(
-          tableAccounts, {"screenName": username, "password": password, "email": email, "authHeader": authHeader});
+      database.insert(tableAccounts,
+          {"screenName": username, "password": password, "email": email, "authHeader": json.encode(authHeader)});
 
       return L10n.current.login_success;
     } else {
