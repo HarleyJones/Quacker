5a25e8b090483325158e6352c66537badf9a4782
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-04-17 19:14:34 +0100

    refactor: minor client changes

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -29,8 +29,6 @@ class _QuackerTwitterClient extends TwitterClient {
     return fetch(uri, headers: headers).timeout(timeout ?? _defaultTimeout).then((response) {
       if (response?.statusCode != null && response!.statusCode >= 200 && response.statusCode < 300) {
         return response;
-      } else if (response?.statusCode != null) {
-        return Future.error(jsonDecode(response!.body)['errors'][0]['message'].toString().replaceAll('.', ''));
       } else {
         return Future.error(HttpException("${response?.statusCode}: ${response?.reasonPhrase}"));
       }
@@ -42,11 +40,7 @@ class _QuackerTwitterClient extends TwitterClient {
     var authHeader = await getAuthHeader(prefs);
 
     if (authHeader != null) {
-      try {
-        return await fetchAuthenticated(uri, headers: headers, log: log, prefs: prefs, authHeader: authHeader);
-      } catch (_) {
-        return await fetchUnauthenticated(uri, headers: headers, log: log);
-      }
+      return await fetchAuthenticated(uri, headers: headers, log: log, prefs: prefs, authHeader: authHeader);
     } else {
       return await fetchUnauthenticated(uri, headers: headers, log: log);
     }
@@ -535,7 +529,7 @@ class Twitter {
     bool showPinnedTweet = true;
     Map<String, Object> defaultUserTweetsParam = {
       "variables":
-          "{\"userId\":\"160534877\",\"count\":20,\"includePromotedContent\":true,\"withQuickPromoteEligibilityTweetFields\":true,\"withVoice\":true,\"withV2Timeline\":true}",
+          "{\"userId\":\"160534877\",\"count\":$count,\"includePromotedContent\":true,\"withQuickPromoteEligibilityTweetFields\":true,\"withVoice\":true,\"withV2Timeline\":true}",
       "features":
           "{\"rweb_lists_timeline_redesign_enabled\":true,\"responsive_web_graphql_exclude_directive_enabled\":true,\"verified_phone_label_enabled\":false,\"creator_subscriptions_tweet_preview_api_enabled\":true,\"responsive_web_graphql_timeline_navigation_enabled\":true,\"responsive_web_graphql_skip_user_profile_image_extensions_enabled\":false,\"tweetypie_unmention_optimization_enabled\":true,\"responsive_web_edit_tweet_api_enabled\":true,\"graphql_is_translatable_rweb_tweet_is_translatable_enabled\":true,\"view_counts_everywhere_api_enabled\":true,\"longform_notetweets_consumption_enabled\":true,\"responsive_web_twitter_article_tweet_consumption_enabled\":false,\"tweet_awards_web_tipping_enabled\":false,\"freedom_of_speech_not_reach_fetch_enabled\":true,\"standardized_nudges_misinfo\":true,\"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled\":true,\"longform_notetweets_rich_text_read_enabled\":true,\"longform_notetweets_inline_media_enabled\":true,\"responsive_web_media_download_video_enabled\":false,\"responsive_web_enhance_cards_enabled\":false}",
       "fieldToggles": "{\"withAuxiliaryUserLabels\":false,\"withArticleRichContentState\":false}"
@@ -546,7 +540,6 @@ class Twitter {
     if (cursor != null) {
       variables['cursor'] = cursor;
     }
-    variables['count'] = count;
     defaultUserTweetsParam["variables"] = json.encode(variables);
 
     var response = await _twitterApi.client
