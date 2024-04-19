f8e4300ef284f78f3d3e076f97fd0f248b3ef7e7
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-04-19 21:20:03 +0100

    add: nice settings screen

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:quacker/generated/l10n.dart';
 import 'package:quacker/settings/_about.dart';
@@ -46,80 +47,104 @@ class _SettingsScreenState extends State<SettingsScreen> {
       body: ListView(
         padding: EdgeInsets.all(16.0),
         children: [
-          ExpansionTile(
-              title: Text(
-                L10n.of(context).settings,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              initiallyExpanded: true,
-              dense: true,
-              trailing: SizedBox.shrink(),
-              enabled: false,
-              children: [
-                ListTile(
-                  title: Text(L10n.of(context).general),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SettingsGeneralFragment()),
-                  ),
-                ),
-                ListTile(
-                  title: Text(L10n.of(context).account),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SettingsAccountFragment()),
-                  ),
-                ),
-                ListTile(
-                  title: Text(L10n.of(context).home),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SettingsHomeFragment()),
-                  ),
-                ),
-                ListTile(
-                  title: Text(L10n.of(context).theme),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SettingsThemeFragment()),
-                  ),
-                ),
-                ListTile(
-                  title: Text(L10n.of(context).accessibility),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SettingsAccessibilityFragment()),
-                  ),
-                ),
-              ]),
+          ListTile(
+            title: Text(L10n.of(context).general),
+            leading: Icon(Icons.miscellaneous_services),
+            subtitle: Text(
+              "${L10n.of(context).language}, ${L10n.of(context).should_check_for_updates_label}, ${L10n.of(context).default_tab}, ${L10n.of(context).media_size}, ${L10n.of(context).mute_videos}, ${L10n.of(context).hide_sensitive_tweets}, ${L10n.of(context).share_base_url}, ${L10n.of(context).disable_screenshots}, ${L10n.of(context).download_handling}, ${L10n.of(context).activate_non_confirmation_bias_mode_label}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SettingsGeneralFragment()),
+            ),
+          ),
+          ListTile(
+            title: Text(L10n.of(context).account),
+            leading: Icon(Icons.account_circle),
+            subtitle: Text(
+              "${L10n.of(context).account}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SettingsAccountFragment()),
+            ),
+          ),
+          ListTile(
+            title: Text(L10n.of(context).home),
+            leading: Icon(Icons.home),
+            subtitle: Text(
+              "${L10n.of(context).reset_home_pages}, ${L10n.of(context).home}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SettingsHomeFragment()),
+            ),
+          ),
+          ListTile(
+            title: Text(L10n.of(context).theme),
+            subtitle: Text(
+              "${L10n.of(context).theme_mode}, ${L10n.of(context).theme}, ${L10n.of(context).true_black}, ${L10n.of(context).show_navigation_labels}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            leading: Icon(Icons.palette),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SettingsThemeFragment()),
+            ),
+          ),
+          ListTile(
+            title: Text(L10n.of(context).accessibility),
+            leading: Icon(Icons.settings_accessibility),
+            subtitle: Text(
+              "${L10n.of(context).disable_animations}",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SettingsAccessibilityFragment()),
+            ),
+          ),
           ExpansionTile(
               title: Text(
                 L10n.of(context).data,
                 style: Theme.of(context).textTheme.titleMedium,
               ),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+              trailing: const SizedBox.shrink(),
               initiallyExpanded: true,
-              dense: true,
-              trailing: SizedBox.shrink(),
               enabled: false,
+              backgroundColor: Theme.of(context).colorScheme.onPrimary,
               children: [
                 SettingsDataFragment(
                   legacyExportPath: _legacyExportPath,
                 )
               ]),
+          SizedBox(
+            height: 8.0,
+          ),
           ExpansionTile(
               title: Text(
                 L10n.of(context).app_info,
                 style: Theme.of(context).textTheme.titleMedium,
               ),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+              trailing: const SizedBox.shrink(),
               initiallyExpanded: true,
-              dense: true,
-              trailing: SizedBox.shrink(),
               enabled: false,
+              backgroundColor: Theme.of(context).colorScheme.onSecondary,
               children: [
                 SettingsAboutFragment(
                   appVersion: appVersion,
