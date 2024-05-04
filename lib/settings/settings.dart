37a18d6ef662c83714103c4ee548727f76f59e8a
mrhcjones <150591975+TheHCJ@users.noreply.github.com>
2024-05-04 15:47:11 +0100

    convert expansion tiles into cards + add glitchtip settings preview

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -50,7 +50,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
             title: Text(L10n.of(context).general),
             leading: Icon(Icons.miscellaneous_services),
             subtitle: Text(
-              "${L10n.of(context).language}, ${L10n.of(context).should_check_for_updates_label}, ${L10n.of(context).default_tab}, ${L10n.of(context).media_size}, ${L10n.of(context).mute_videos}, ${L10n.of(context).hide_sensitive_tweets}, ${L10n.of(context).share_base_url}, ${L10n.of(context).disable_screenshots}, ${L10n.of(context).download_handling}, ${L10n.of(context).activate_non_confirmation_bias_mode_label}",
+              "${L10n.of(context).language}, ${L10n.of(context).should_check_for_updates_label}, ${L10n.of(context).default_tab}, ${L10n.of(context).media_size}, ${L10n.of(context).mute_videos}, ${L10n.of(context).hide_sensitive_tweets}, ${L10n.of(context).share_base_url}, ${L10n.of(context).disable_screenshots}, ${L10n.of(context).download_handling}, ${L10n.of(context).activate_non_confirmation_bias_mode_label}, ${L10n.of(context).enable_glitchtip}",
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
               style: TextStyle(fontStyle: FontStyle.italic),
@@ -119,35 +119,33 @@ class _SettingsScreenState extends State<SettingsScreen> {
               MaterialPageRoute(builder: (context) => const SettingsAccessibilityFragment()),
             ),
           ),
-          ExpansionTile(
-              title: Text(
-                L10n.of(context).data,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-              trailing: const SizedBox.shrink(),
-              initiallyExpanded: true,
-              enabled: false,
-              backgroundColor: Theme.of(context).colorScheme.onPrimary,
-              children: const [SettingsDataFragment()]),
-          SizedBox(
+          Card(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Column(children: [
+                ListTile(
+                  title: Text(
+                    L10n.of(context).data,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                SettingsDataFragment()
+              ])),
+          const SizedBox(
             height: 8.0,
           ),
-          ExpansionTile(
-              title: Text(
-                L10n.of(context).app_info,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-              trailing: const SizedBox.shrink(),
-              initiallyExpanded: true,
-              enabled: false,
-              backgroundColor: Theme.of(context).colorScheme.onSecondary,
-              children: [
+          Card(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              child: Column(children: [
+                ListTile(
+                  title: Text(
+                    L10n.of(context).app_info,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
                 SettingsAboutFragment(
                   appVersion: appVersion,
                 )
-              ]),
+              ])),
         ],
       ),
     );
